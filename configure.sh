065ff75bbcc3075e186287ec8f29e04f798470ce
Antonio Decanini <antonio.decanini@hotmail.com>
2020-08-12 19:58:09 -0300

    Add Docker instalation to configure.sh

--- configure.sh
+++ configure.sh
@@ -68,3 +68,26 @@ code --install-extension ms-azuretools.vscode-docker
 
 ## Replace vsCode configs
 cp configs/settings.json ~/.config/Code/User/settings.json
+
+## Install Docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Ass user to Docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
